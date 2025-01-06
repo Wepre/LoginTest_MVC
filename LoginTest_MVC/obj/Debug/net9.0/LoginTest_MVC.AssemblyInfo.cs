bf2f9138fff297c5c04bb653d5a4fdc0405f8c66
FINNIAN X <1771555693@qq.com>
2025-01-06 11:34:12 +0800

    revert了无用的提交

--- LoginTest_MVC/obj/Debug/net9.0/LoginTest_MVC.AssemblyInfo.cs
+++ LoginTest_MVC/obj/Debug/net9.0/LoginTest_MVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoginTest_MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd6b147e01a15cea701b75b5d8c77a886bb20e50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd988df79ead8fa360c514bb31712cd8eaa3e24d")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoginTest_MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoginTest_MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- LoginTest_MVC/repository/IPersonRepository.cs
+++ LoginTest_MVC/repository/IPersonRepository.cs
@@ -6,7 +6,6 @@ namespace LoginTest_MVC.repository;
 public interface IPersonRepository
 {
     List<PersonViewModel> _personViewModels { get; set; }
-    //改动第一个一的一
     PersonViewModel GetPersonViewModel(string  username);
     void AddPersonViewModel(StringValues username, StringValues password);
     List<PersonViewModel> GetAllPersonViewModel();
