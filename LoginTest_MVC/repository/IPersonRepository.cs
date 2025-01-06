226f32700170b701b8dc25259c4cc272d3c6d12b
FINNIAN X <1771555693@qq.com>
2025-01-06 11:48:33 +0800

    fixup跟普通的提交有什么区别？

--- LoginTest_MVC/repository/IPersonRepository.cs
+++ LoginTest_MVC/repository/IPersonRepository.cs
@@ -6,7 +6,7 @@ namespace LoginTest_MVC.repository;
 public interface IPersonRepository
 {
     List<PersonViewModel> _personViewModels { get; set; }
-    //改动又来了一，fixup是什么意思？fixup就是追加要提交的内容，可能上一个提交当中，是还不够完善的，可以多文件吧‘
+    //改动又来了一，fixup是什么意思？fixup就是追加要提交的内容，可能上一个提交当中，是还不够完善的，可以多文件吧‘111
     
     PersonViewModel GetPersonViewModel(string  username);
     void AddPersonViewModel(StringValues username, StringValues password);
