3115279c81623eb21b8457c37be5ccdc1da8e9e6
FINNIAN X <1771555693@qq.com>
2025-01-07 10:04:28 +0800

    添加.gitignore文件及更新AssemblyInfo版本

--- LoginTest_MVC/obj/Debug/net9.0/LoginTest_MVC.AssemblyInfo.cs
+++ LoginTest_MVC/obj/Debug/net9.0/LoginTest_MVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoginTest_MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd988df79ead8fa360c514bb31712cd8eaa3e24d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+226f32700170b701b8dc25259c4cc272d3c6d12b")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoginTest_MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoginTest_MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
